9da76c7d76c92ea24bb5f273fad60c839f703be1
Daniel Boyle <boylede@gmail.com>
2019-12-04 21:33:24 -0800

    move interesting code to top

--- src/day5/mod.rs
+++ src/day5/mod.rs
@@ -7,45 +7,8 @@ use aoc2019::Day;
 
 const DAY: i32 = 5;
 
-pub fn load(days_array: &mut Vec<Day>) {
-    days_array.push(Day::new(DAY, run));
-}
-
-pub fn run(input: File) {
-    println!("loading day {} input.", DAY);
-    let a_time = time::precise_time_ns();
-
-    let mut lines = vec![];
-    {
-        let mut lines_iterator = BufReader::new(&input).lines();
-        while let Some(Ok(line)) = lines_iterator.next() {
-            lines.push(line);
-        }
-    }
-    let b_time = time::precise_time_ns();
-    let total_time = b_time - a_time;
-    if total_time > 100000 {
-        println!("Loading took: {}ms", total_time as f32 / 1000000.0);
-    } else {
-        println!("Loading took: {}ns", total_time);
-    }
-
-    post_load(lines);
-}
-
-fn post_load(lines: Vec<String>) {
-    let a_time = time::precise_time_ns();
-    part1(&lines);
-    let b_time = time::precise_time_ns();
-    part2(&lines);
-    let c_time = time::precise_time_ns();
-    println!("Day {} Part 1 took: {}ns", DAY, b_time - a_time);
-    println!("Day {} Part 2 took: {}ns", DAY, c_time - b_time);
-}
-
 fn part1(lines: &Vec<String>) {
     let mut program: Program = lines[0].parse().unwrap();
-    // program.inject_input(12, 2);
     program.input(1);
     program.execute();
     let value = program.output;
@@ -54,26 +17,13 @@ fn part1(lines: &Vec<String>) {
 
 fn part2(lines: &Vec<String>) {
     let mut program: Program = lines[0].parse().unwrap();
-    // program.inject_input(12, 2);
     program.input(5);
     program.execute();
     let value = program.output;
     println!("Part 2: {:?}", value);
-
-    // let program: Program = lines[0].parse().unwrap();
-    // for noun in 0..=99 {
-    //     for verb in 0..=99 {
-    //         let mut attempt = program.clone();
-    //         attempt.inject_input(noun, verb);
-    //         attempt.execute();
-    //         let value = attempt.output();
-    //         if value == 19690720 {
-    //             println!("Part 2: {}", noun * 100 + verb);
-    //             return;
-    //         }
-    //     }
-    // }
 }
+
+
 #[derive(Clone)]
 struct Program {
     counter: usize,
@@ -245,6 +195,44 @@ impl FromStr for Program {
         ))
     }
 }
+
+pub fn load(days_array: &mut Vec<Day>) {
+    days_array.push(Day::new(DAY, run));
+}
+
+pub fn run(input: File) {
+    println!("loading day {} input.", DAY);
+    let a_time = time::precise_time_ns();
+
+    let mut lines = vec![];
+    {
+        let mut lines_iterator = BufReader::new(&input).lines();
+        while let Some(Ok(line)) = lines_iterator.next() {
+            lines.push(line);
+        }
+    }
+    let b_time = time::precise_time_ns();
+    let total_time = b_time - a_time;
+    if total_time > 100000 {
+        println!("Loading took: {}ms", total_time as f32 / 1000000.0);
+    } else {
+        println!("Loading took: {}ns", total_time);
+    }
+
+    post_load(lines);
+}
+
+fn post_load(lines: Vec<String>) {
+    let a_time = time::precise_time_ns();
+    part1(&lines);
+    let b_time = time::precise_time_ns();
+    part2(&lines);
+    let c_time = time::precise_time_ns();
+    println!("Day {} Part 1 took: {}ns", DAY, b_time - a_time);
+    println!("Day {} Part 2 took: {}ns", DAY, c_time - b_time);
+}
+
+
 
 #[test]
 pub fn tests() {
