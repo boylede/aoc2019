f8a141deb58e514119a26e6d7acfa4337df18d29
Daniel Boyle <boylede@gmail.com>
2019-12-05 20:15:01 -0800

    add tests

--- src/day5/mod.rs
+++ src/day5/mod.rs
@@ -232,14 +232,24 @@ fn post_load(lines: Vec<String>) {
     println!("Day {} Part 2 took: {}ns", DAY, c_time - b_time);
 }
 
-
-
 #[test]
 pub fn tests() {
     pub fn run(test: &str) -> Vec<i32> {
         let mut program: Program = test.parse().unwrap();
         program.execute();
+        if program.status != RunStatus::Finished {
+            panic!("virtual machine terminated early");
+        }
         program.memory
+    }
+    pub fn io_test(program: &str, input: i32) -> i32 {
+        let mut program: Program = program.parse().unwrap();
+        program.input(input);
+        program.execute();
+        if program.status != RunStatus::Finished {
+            panic!("virtual machine terminated early");
+        }
+        program.output[0]
     }
 
     assert_eq!(run("1,0,0,0,99"), vec![2, 0, 0, 0, 99]);
@@ -249,4 +259,20 @@ pub fn tests() {
         run("1,1,1,4,99,5,6,0,99"),
         vec![30, 1, 1, 4, 2, 5, 6, 0, 99]
     );
+
+    assert_eq!(io_test("3,0,4,0,99", -9854), -9854);
+    assert_eq!(io_test("3,9,8,9,10,9,4,9,99,-1,8", 8), 1);
+    assert_eq!(io_test("3,9,7,9,10,9,4,9,99,-1,8", -6), 1);
+    assert_eq!(io_test("3,3,1108,-1,8,3,4,3,99", 8), 1);
+    assert_eq!(io_test("3,3,1107,-1,8,3,4,3,99", -8), 1);
+
+    assert_eq!(io_test("3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9", -9), 1);
+    assert_eq!(io_test("3,3,1105,-1,9,1101,0,0,12,4,12,99,1", -9), 1);
+
+    // todo: tests fail if there are linebreaks in input;
+    let eight_tester = "3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99";
+
+    assert_eq!(io_test(eight_tester, -9), 999);
+    assert_eq!(io_test(eight_tester, 8), 1000);
+    assert_eq!(io_test(eight_tester, 9000), 1001);
 }
