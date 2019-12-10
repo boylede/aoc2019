beb77689c4fe70e23cc560378c972a24ea651ab1
Daniel Boyle <boylede@gmail.com>
2019-12-09 23:46:12 -0800

    day 10 part 1

--- src/day10/mod.rs
+++ src/day10/mod.rs
@@ -8,29 +8,66 @@ use aoc2019::Day;
 
 const DAY: i32 = 10;
 
-fn check_visible(space: &HashSet<(usize, usize)>, x: usize, y: usize) -> bool {
-    if let Some(asteroid) = space.get(&(x, y)) {
+fn check_visible(space: &HashSet<(i32, i32)>, point: (i32, i32)) -> bool {
+    if let Some(_) = space.get(&point) {
         true
     } else {
         false
     }
 }
-fn hits(space: &HashSet<(usize, usize)>, origin: (usize, usize), target: (usize, usize)) -> bool {
-    unimplemented!()
+
+fn lerp(start: i32, end: i32, t: f32) -> i32 {
+    let start = start as f32;
+    let end = end as f32;
+    (start + t * (end - start)) as i32
 }
 
+fn lerp_point(start: (i32, i32), end: (i32, i32), t: f32) -> (i32, i32) {
+    (lerp(start.0, end.0, t), lerp(start.1, end.1, t))
+}
+fn dist(start: (i32, i32), end: (i32, i32)) -> i32 {
+    let dx = end.0 - start.0;
+    let dy = end.1 - start.1;
+    std::cmp::max(dx, dy)
+}
+
+fn hits(space: &HashSet<(i32, i32)>, origin: (i32, i32), target: (i32, i32)) -> Option<(i32, i32)> {
+    // let (ox, oy) = origin;
+    // let (tx, ty) = target;
+    // let ray = (ox - tx, oy - ty);
+    // for divisor in 1..52 {
+    //     let (rx, ry) = (ray.0 / divisor, ray.1 / divisor);
+    //     let point = (rx + tx, ry + ty);
+    //     if check_visible(space, point) {
+    //         return true
+    //     }
+    // }
+    // println!("{:?}", ray);
+    let mut points = vec![];
+    let max_points = dist(origin, target) + 1;
+    for step in 1..max_points {
+        let t = step as f32 / max_points as f32;
+        points.push(lerp_point(origin, target, t));
+    }
+    for point in points {
+        if check_visible(space, point) {
+            return Some(point);
+        }
+    }
+    None
+}
 
 fn part1(lines: &Vec<String>) {
-    let space : HashSet<(usize, usize)> = lines
+    let space : HashSet<(i32, i32)> = lines
     .iter()
     .enumerate()
     .flat_map(|(y, line)| {
-        let h: HashSet<(usize, usize)> = line
+        let h: HashSet<(i32, i32)> = line
         .chars()
         .enumerate()
         .filter_map(|(x, c) | {
             if c == '#' {
-                Some((x, y))
+                Some((x as i32, y as i32))
             } else {
                 None
             }
@@ -39,31 +76,54 @@ fn part1(lines: &Vec<String>) {
         h
     }).collect();
 
-    for y in 0..36 {
-        for x in 0..36 {
-            if check_visible(&space, x, y) {
-                print!("#");
-            } else {
-                print!(" ");
+    let mut matches : HashMap<(i32, i32), u32> = HashMap::new();
+    for asteroid in space.iter() {
+        let mut visible: HashSet<(i32, i32)> = HashSet::new();
+        for y in 0..36 {
+            for x in 0..36 {
+                let x = x as i32;
+                let y = y as i32;
+                if (x == 0 || x == 35) || (y == 0 || y == 35) {
+                    // print!("{:?}, {},{} = ", *asteroid, x, y);
+                    if let Some(hit) = hits(&space, *asteroid, (x, y)) {
+                        visible.insert(hit);        
+                    }
+                }
+                // if check_visible(&space, x, y) {
+                //     print!("#");
+                // } else {
+                //     print!(" ");
+                // }
             }
+            // print!("\n");
         }
-        print!("\n");
+        let count = visible.len() as u32;
+        matches.insert(*asteroid, count);  //.entry(*asteroid).and_modify(|c| *c = *c + 1).or_insert(1);
     }
+    let best = matches.iter().fold(0, |best, (k, v)| {
+        println!("{:?}: {}", k, v);
+        if *v > best {
+            *v
+        } else {
+            best
+        }
+    });
     // let asteroids :HashSet= space.iter().filter_map(|(k, v)| {
     //     if v {
     //         k
     //     }
     // })
-    let mut matches : HashMap<(usize, usize), u32> = HashMap::new();
-    for asteroid in space.iter() {
-        for y in 0..36*2 {
-            for x in 0..36*2 {
-                if hits(&space, *asteroid, (x, y)) {
-                    matches.entry((x, y)).and_modify(|c| *c = *c + 1).or_insert(1);
-                }
-            }
-        }
-    }
+
+    // let mut matches : HashMap<(usize, usize), u32> = HashMap::new();
+    // for asteroid in space.iter() {
+    //     for y in 0..36*2 {
+    //         for x in 0..36*2 {
+    //             if hits(&space, *asteroid, (x, y)) {
+    //                 matches.entry((x, y)).and_modify(|c| *c = *c + 1).or_insert(1);
+    //             }
+    //         }
+    //     }
+    // }
 
     // space.iter().for_each(|t| {
     //     t.iter().for_each(|pa| {
@@ -96,22 +156,22 @@ fn part1(lines: &Vec<String>) {
     //     }
     // });
 
-    println!("Part 1: {:?}", 0);
+    println!("Part 1: {:?}", best);
 }
 
-fn print_image(image: &Vec<u32>, width: usize, height: usize) {
-    for y in 0..height {
-        for x in 0..width {
-            if image[x + y * width] == 0 {
-                print!(" ");
-            } else {
-                print!("█");
-            }
-        }
-        print!("\n");
-    }
-    print!("\n\n");
-}
+// fn print_image(image: &Vec<u32>, width: usize, height: usize) {
+//     for y in 0..height {
+//         for x in 0..width {
+//             if image[x + y * width] == 0 {
+//                 print!(" ");
+//             } else {
+//                 print!("█");
+//             }
+//         }
+//         print!("\n");
+//     }
+//     print!("\n\n");
+// }
 
 fn part2(lines: &Vec<String>) {
     // let pixels = lines[0]
